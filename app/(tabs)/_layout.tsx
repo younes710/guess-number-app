72d9daa7861e67540ccde2d3d064e13d12149fca
Younes Abdi <realyoonesabdi@gmail.com>
2024-08-02 16:59:31 +0330

    Create react native app

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,27 @@
+import { Tabs } from 'expo-router';
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+ const colorScheme = useColorScheme();
+
+ return (
+  <Tabs
+   screenOptions={{
+    tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+    headerShown: false,
+   }}
+  >
+   <Tabs.Screen
+    name='index'
+    options={{
+     title: 'Home',
+     tabBarIcon: ({ color, focused }) => (
+      <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+     ),
+    }}
+   />
+  </Tabs>
+ );
+}
